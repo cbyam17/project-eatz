1eec7058cf9f07a15ed0cab56595bcb03ce29f5f
cbyam <cbyam@insulet.com>
2021-10-25 14:43:03 -0400

    removed 'require' keyword

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window._config = {
+    api: {
+        invokeUrl: 'https://d8qga9j6ob.execute-api.us-east-1.amazonaws.com/dev',
+        authToken: '0eb6b64d-4aee-40d9-908d-4846044ee0f0'
+    }
+};
